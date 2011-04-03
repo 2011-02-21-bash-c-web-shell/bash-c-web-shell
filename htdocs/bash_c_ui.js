a1d9a84799fd6ed36ae4b972fb37a0ae90a561e5
Andrej A Antonov <polymorphm@gmail.com>
2011-04-03 19:06:05 +0400

    licence headers fixes

--- htdocs/bash_c_ui.js
+++ htdocs/bash_c_ui.js
@@ -1,5 +1,7 @@
 // -*- mode: javascript; coding: utf-8 -*-
-
+//
+// Copyright 2011 Andrej A Antonov <polymorphm@gmail.com>
+// 
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Lesser General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
